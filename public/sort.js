1eed6acb3f67fe210c4098d4df28046fba7537ae
Daniel Zwelling <dzwellingmusic@gmail.com>
2016-01-26 14:31:44 -0800

    refactor sort.js to compare with user list

--- public/sort.js
+++ public/sort.js
@@ -12,5 +12,13 @@ function matchMaker(meatHead1, meatHead2) {
 
 var joe = {'day': 'tuesday', 'time': '6 AM', 'location': 'anytime fitness'};
 var brady = {'day': 'monday', 'time': '6 AM', 'location': 'anytime fitness'};
+var users = [joe, brady];
 
+console.log(users);
 console.log(matchMaker(joe, brady));
+
+//Change params to (meathead, userList)
+//Rotate list so meathead is at userList[0]
+//Start comparison from userList[1]
+//Push like users to new array and return new array
+
