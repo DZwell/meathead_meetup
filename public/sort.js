d2da69a9abad9f278568eca3c7b63206f96d9b88
Daniel Zwelling <dzwellingmusic@gmail.com>
2016-01-26 14:16:06 -0800

    Modify sort

--- public/sort.js
+++ public/sort.js
@@ -1,11 +1,16 @@
 
 
 function matchMaker(meatHead1, meatHead2) {
-    return JSON.stringify(meatHead1) === JSON.stringify(meatHead2);
+    if (JSON.stringify(meatHead1) === JSON.stringify(meatHead2)) {
+        console.log('We found a match!');
+        return '';
+    }
+    console.log('Sorry, no one is available to work out at this time.');
+    return '';
 }
 
 
-var joe = {'day': 'monday', 'time': '6 AM', 'location': 'anytime fitness'};
+var joe = {'day': 'tuesday', 'time': '6 AM', 'location': 'anytime fitness'};
 var brady = {'day': 'monday', 'time': '6 AM', 'location': 'anytime fitness'};
 
 console.log(matchMaker(joe, brady));
