66327223496529b503660f1e841687a22fc586aa
Daniel Zwelling <dzwellingmusic@gmail.com>
2016-01-27 16:31:24 -0800

    add event handler and AJAX

--- public/js/events.js
+++ public/js/events.js
@@ -1,10 +1,27 @@
 $(function(){
+    $('button').click('submit', function(event) {
+        event.preventDefault();
 
-    $('form :input').on('click', function() {
-        console.log($('form').val);
-    });
+        var newUser = {
+            0: $('#userName').val(),
+            1: $('#day').val(),
+            2: $('#times').val(),
+            3: $('#gym').val(),
+            4: $('#muscle').val(),
+        };
+        users.push(newUser);
+        $('.properties').val('');
+        console.log(users);
 
-
-
-
-});
+        // $.ajax({
+        //     type: 'POST',
+        //     data: {
+        //         name: $('#userName').val(),
+        //         day: $('#day').val(),
+        //         times: $('#times').val(),
+        //         gym: $('#gym').val(),
+        //         muscleGroup: $('#muscle').val(),
+        //     } //data
+        // }); //ajax
+    }); //event
+}); //jquery
