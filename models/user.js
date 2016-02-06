5b02c3b4a0a76460ae708798ea2f8ee66ef31b4a
Mike Skalandunas <mskalandunas@gmail.com>
2016-02-05 19:04:07 -0800

    comment out all bcrypt/eat related backend and add users to be workable without authroutes

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@
 
 var mongoose = require('mongoose');
 // var bcrypt = require('bcrypt');
-var eat = require('eat');
+// var eat = require('eat');
 
 var userSchema = new mongoose.Schema({
   name: {
--- routes/users_routes.js
+++ routes/users_routes.js
@@ -3,12 +3,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var User = require(__dirname + '/../models/user');
-var eatAuth = require(__dirname + '/../lib/eat_auth');
+// var eatAuth = require(__dirname + '/../lib/eat_auth');
 var handleError = require(__dirname + '/../lib/handle_error');
 
 var usersRouter = module.exports = exports = express.Router();
 
-usersRouter.get('/users', eatAuth, function(req, res) {
+usersRouter.get('/users', function(req, res) {
   User.find({}, function(err, data) {
     if (err) return handleError(err, res);
 
@@ -16,20 +16,29 @@ usersRouter.get('/users', eatAuth, function(req, res) {
   });
 });
 
-usersRouter.put('/users/:id', bodyParser.json(), eatAuth, function(req, res) {
+usersRouter.post('/users', function(req, res) {
+  var newUser = new User(req.body);
+  newUser.save(function(err, data) {
+    if (err) return handleError(err, res);
+
+    res.json({success: true, msg: 'Meathead added.'});
+  });
+});
+
+usersRouter.put('/users/:id', bodyParser.json(), function(req, res) {
   var userData = req.body;
   delete userData._id;
   User.update({_id: req.params.id}, userData, function(err) {
     if (err) return handleError(err, res);
 
-    res.json({msg: 'A new user joins the search for a buddy!'});
+    res.json({msg: 'Meathead edited.'});
   });
 });
 
-usersRouter.delete('/users/:id', eatAuth, function(req, res) {
+usersRouter.delete('/users/:id', function(req, res) {
   User.remove({_id: req.params.id}, function(err) {
     if (err) return handleError(err, res);
 
-    res.json({msg: 'User removed.'});
+    res.json({msg: 'Meathead deleted.'});
   });
 });
