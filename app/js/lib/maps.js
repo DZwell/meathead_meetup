2b5e1a52bc6b6fc18e45df07b407b1512d9eac6c
Daniel Zwelling <dzwellingmusic@gmail.com>
2016-02-21 03:10:47 -0800

    Working auto complete for address and gym. Boomshakalaka

--- app/js/lib/maps.js
+++ app/js/lib/maps.js
@@ -1,20 +1,24 @@
-var map;
-var autocomplete;
-var service;
+// var map;
+// var autocomplete;
+// var service;
+// var marker;
 
 function initMap() {
-  var usa = {lat: 39.062, lng: -101.778};
-  var infowindow = new google.maps.InfoWindow({content: ''});
-  var geocoder = new google.maps.Geocoder();
-  // var autocomplete = new google.maps.places.Autocomplete($('#user-location-search'));
-  // autocomplete.bindTo('bounds', map);
-
   map = new google.maps.Map(document.getElementById('map'), {
-    center: usa,
+    center: {lat: 39.062, lng: -101.778},
     scrollwheel: false,
     zoom: 3,
     scaleControl: true
   });
+
+  var infowindow = new google.maps.InfoWindow({content: ''});
+  var geocoder = new google.maps.Geocoder();
+  var service = new google.maps.places.PlacesService(map);
+  var input = document.getElementById('user-location-search');
+  var types = document.getElementById('form_container');
+  var autocomplete = new google.maps.places.Autocomplete(input);
+  var marker = new google.maps.Marker();
+
 
   $('#submit').on('click', function(e) {
     e.preventDefault();
@@ -22,7 +26,7 @@ function initMap() {
       geocodeAddress(geocoder, map);
     }
     if ($('#place').is(':checked')){
-      clickListener(['establishment'])
+      setupClickListener(['establishment'])
     }
     map.setZoom(13);
   });
@@ -60,7 +64,6 @@ function initMap() {
   }
 
   function createMarker(place) {
-    var service = new google.maps.places.PlacesService(map);
     var placeLoc = place.geometry.location;
     var marker = new google.maps.Marker({
       map: map,
@@ -77,14 +80,65 @@ function initMap() {
           infowindow.open(map, marker);
         }
       });
-    })
+    }); //End marker event listener
+  } //End createMarker
+
+
+    autocomplete.bindTo('bounds', map);
+    var marker = new google.maps.Marker({
+      map: map,
+      anchorPoint: new google.maps.Point(0, -29)
+    });
+
+
+    autocomplete.addListener('place_changed', function() {
+      infowindow.close();
+      marker.setVisible(false);
+      var place = autocomplete.getPlace();
+      if (!place.geometry) {
+        window.alert("Autocomplete's returned place contains no geometry");
+        return;
+      }
+
+      if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+      }
+      else {
+        map.setCenter(place.geometry.location);
+        map.setZoom(17);  // Why 17? Because it looks good.
+      }
+      marker.setIcon(/** @type {google.maps.Icon} */({
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(35, 35)
+    }));
+    marker.setPosition(place.geometry.location);
+    marker.setVisible(true);
+
+    var address = '';
+    if (place.address_components) {
+      address = [
+        (place.address_components[0] && place.address_components[0].short_name || ''),
+        (place.address_components[1] && place.address_components[1].short_name || ''),
+        (place.address_components[2] && place.address_components[2].short_name || '')
+      ].join(' ');
+    }
+
+    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+    infowindow.open(map, marker);
+  }); //End autocomplete event listener
+
+
+  // Sets a listener on a radio button to change the filter type on Places
+  // Autocomplete.
+  function setupClickListener(id, types) {
+    var radioButton = document.getElementById(id);
+    radioButton.addEventListener('click', function() {
+      autocomplete.setTypes(types);
+    });
   }
-
-
-  function clickListener(id, types) {
-
-  }
-
-};
+}
 
 
