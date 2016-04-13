d2a15f4b9bead975b28fa72bc03cca651db16819
Mike Skalandunas <mskalandunas@gmail.com>
2016-04-13 01:42:40 -0700

    changes to map, header

--- app/js/map/map.js
+++ app/js/map/map.js
@@ -2,4 +2,6 @@
 
 module.exports = function(app) {
   require('./controllers/map_controller')(app);
+  require('./directives/search-directive')(app);
+  require('./directives/search-view-directive')(app);
 };
