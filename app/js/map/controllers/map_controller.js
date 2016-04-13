00a106ef8f7eb4c0494a248504b2acf093a66543
Mike Skalandunas <mskalandunas@gmail.com>
2016-04-13 00:05:53 -0700

    map showing

---
+++ app/js/map/controllers/map_controller.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = function(app) {
+  app.controller('MapController', function(NgMap) {
+    NgMap.getMap().then(function(map) {
+      console.log(map.getCenter());
+      console.log('markers', map.markers);
+      console.log('shapes', map.shapes);
+    });
+  });
+};
---
+++ app/js/map/map.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = function(app) {
+  require('./controllers/map_controller')(app);
+};
