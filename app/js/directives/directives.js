b65a2fa638133820d9a186000fcc0bfff52038bd
Mike Skalandunas <mskalandunas@gmail.com>
2016-04-13 21:51:06 -0700

    spotter request form created

--- app/js/directives/directives.js
+++ app/js/directives/directives.js
@@ -3,5 +3,6 @@
 module.exports = function(app) {
   require('./footer-directive')(app);
   require('./header-directive')(app);
+  require('./test-directive')(app); // delete later
   require('./home-view-directive.js')(app);
 };
